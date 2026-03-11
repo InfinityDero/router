ff3ca63595868aea8c8bad8fe16c21993d5b560f
ron <ron.abramovitz2007@gmail.com>
2026-03-11 16:42:16 +0200

    corrected MAD ROUTER now it works

--- router.py
+++ router.py
@@ -1,29 +1,14 @@
 from scapy.all import *
+
 FIRST_PACKET = 0
 
 
 def main():
     while(True):
-        packet = sniff()
-        s = packet[FIRST_PACKET]
-        if s[Ether].dst == '08:00:27:9a:ce:39':
-            s[Ether].dst = '0a:00:27:00:00:3A'
-            if ARP in s:
-                s[ARP].pdst = '192.168.240.1'
-                s[ARP].hwsrc = '0a:00:27:00:00:3A'
-            if IP in s:
-                s[IP].dst = '192.168.240.1'
-            sendp(s, iface="enp0s8")
-        else:
-            s[Ether].src = '08:00:27:9a:ce:39'
-            if ARP in s:
-                s[ARP].psrc = '192.168.240.4'
-                s[ARP].hwsrc = '08:00:27:9a:ce:39'
-            if IP in s:
-                s[IP].src = '192.168.240.4'
-            sendp(s, iface="enp0s9")
+        packet = sniff(iface = "enp0s8", count=1)
+    p = packet[FIRST_PACKET]
+        sendp(p, iface = "enp0s9")
 
 
 if __name__ == "__main__":
     main()
-
